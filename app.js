188114f7fe45ca0ba287ae6b4aa203be0d5979db
shubha <shubhampatidar961@gmial.com>
2023-01-25 20:13:50 +0530

    first commit

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import arry from "./componets/constant";
-const Aplayout = () => {
-  const [myarry, setmyarry] = useState(arry);
+import { useState } from "react";
+//import UsestateObject from "./componets/usestateObject";
+const Body = () => {
+  let [email, setemail] = useState();
+  let [password, setpassword] = useState();
+  const Login = () => {
+    let newarryemail = [];
+    let newarrypassword = [];
+    newarryemail.push(email);
+    newarrypassword.push(password);
+    setemail("");
+    setpassword("");
+  };
   return (
-    <>
-      <div className="div-1">
-        {myarry.map((java) => (
-          <h1 className="sk" key={java.id}>
-            Name:{java.name} Age:{java.age}
-          </h1>
-        ))}
-        <button
-          className="btn"
-          onClick={() => {
-            setmyarry([]);
-          }}
-        >
-          clear
-        </button>
+    <div className="contaner">
+      <div className="form">
+        <div>
+          <label>email:</label>
+          <input
+            className="email"
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => {
+              email = e.target.value;
+              setemail(email);
+            }}
+          />
+        </div>
+        <div>
+          <label>password:</label>
+          <input
+            className="email"
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => {
+              password = e.target.value;
+              setpassword(password);
+            }}
+          />
+        </div>
+        <div className="submit">
+          <button className="btn" onClick={Login}>
+            login
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(heading2);
-root.render(<Aplayout />);
+root.render(<Body />);
