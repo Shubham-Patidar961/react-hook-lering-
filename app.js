d270c12a96038c6e3b0b2a3f031199fbf88db33c
shubha <shubhampatidar961@gmial.com>
2023-01-26 17:56:05 +0530

    first commit

--- app.js
+++ app.js
@@ -1,56 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
+//import { useEffect } from "react";
 //import UsestateObject from "./componets/usestateObject";
+let cout = 0;
 const Body = () => {
-  const [email, setemail] = useState();
-  const [password, setpassword] = useState();
-  const [allarry, setallarry] = useState([]);
-  console.log(allarry);
-  const Login = () => {
-    if (email && password) {
-      newarry = { email, password };
-      setallarry([...allarry, newarry]);
-      setemail("");
-      setpassword("");
+  const [change, setChange] = useState(cout);
+  useEffect(() => {
+    if (change === 0) {
+      document.title = `chats`;
     } else {
-      alert("provide input");
+      document.title = `chats(${change})`;
     }
+  });
+  const onclick = () => {
+    setChange((cout = cout + 1));
   };
   return (
-    <div className="contaner">
-      <div className="form">
-        <div className="emaildiv">
-          <label>email:</label>
-          <input
-            className="email"
-            type="text"
-            placeholder="email"
-            value={email}
-            onChange={(e) => {
-              setemail(e.target.value);
-            }}
-          />
-        </div>
-        <div className="passdiv">
-          <label>password:</label>
-          <input
-            className="email"
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => {
-              setpassword(e.target.value);
-            }}
-          />
-        </div>
-        <div className="submit">
-          <button className="btn" onClick={Login}>
-            login
-          </button>
-        </div>
+    <>
+      <div className="sk">
+        <h1>{change}</h1>
+        <button className="btn" onClick={onclick}>
+          click
+        </button>
       </div>
-    </div>
+    </>
   );
 };
 
